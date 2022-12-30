9bdd79eefd07b2ce774e8d19359f88666e567a90
YJinPort <115676636+YJinPort@users.noreply.github.com>
2022-12-30 00:04:40 +0000

    Qt_init_clean_1229
    Qt_init_clean_1229

---
+++ init_clean_1229/dog.h
@@ -0,0 +1,25 @@
+#ifndef DOG_H
+#define DOG_H
+
+#include <QObject>
+#include <QDebug>
+
+class Dog : public QObject
+{
+    Q_OBJECT
+public:
+    explicit Dog(QObject *parent = nullptr);
+
+private slots:
+    void initTestCase();
+    void init();
+    void cleanup();
+    void cleanupTestCase();
+
+    void bark();
+    void rollover();
+
+    void simple();
+};
+
+#endif // DOG_H
